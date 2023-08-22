a04af63b214a418e938f942b22b210c92ec92cf0
KanhaiyaKumarGupta <21125@iiitu.ac.in>
2023-08-22 22:39:22 +0530

    Final Commit

---
+++ app/src/main/java/com/example/menu/CustomMenu.kt
@@ -0,0 +1,49 @@
+package com.example.menu
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.View
+import android.widget.PopupMenu
+import android.widget.Toast
+
+class CustomMenu {
+    @SuppressLint("DiscouragedPrivateApi")
+    fun customMenu(context: Context, view: View)
+    {
+        val pop = PopupMenu(context,view)
+        pop.inflate(R.menu.menu)
+
+        pop.setOnMenuItemClickListener {
+            when(it!!.itemId)
+            {
+                R.id.edit ->{
+                    Toast.makeText(context,"Clicked Edit",Toast.LENGTH_LONG).show()
+                    true
+                }
+                R.id.delete ->{
+                    Toast.makeText(context,"Clicked delete",Toast.LENGTH_LONG).show()
+                    true
+                }
+                R.id.share ->{
+                    Toast.makeText(context,"Clicked delete",Toast.LENGTH_LONG).show()
+                    true
+                }
+
+                else -> {
+                    false
+                }
+            }
+        }
+        try {
+            val fieldMpopup = PopupMenu::class.java.getDeclaredField("mPopup")
+            fieldMpopup.isAccessible = true
+            val mPopup = fieldMpopup.get(pop)
+            mPopup.javaClass.getDeclaredMethod("setForceShowIcon",Boolean::class.java)
+                .invoke(mPopup,true)
+        }catch (_:Exception){
+
+        }finally {
+            pop.show()
+        }
+    }
+}
